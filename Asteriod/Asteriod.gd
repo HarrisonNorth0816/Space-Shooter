94918ac9143dc4700bc3f8938d4af7aef155a177
HarrisonNorth0816 <hanorth@iu.edu>
2023-09-19 11:39:01 -0400

    project 2 part 3 finished

--- Asteriod/Asteriod.gd
+++ Asteriod/Asteriod.gd
@@ -17,5 +17,11 @@ func _physics_process(_delta):
 func damage(d):
 	health -= d
 	if health <= 0:
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instantiate()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+			hide()
+			await explosion.animation_finished
 		queue_free()
-	
--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,6 +1,25 @@
 extends CharacterBody2D
 
 var Bullet = load("res://Enemy/enemy_bullet.tscn")
+var health = 10
+var y_positions = [100, 150, 200, 500, 550]
+var initial_position = Vector2.ZERO
+var direction = Vector2(1.5,0)
+var wobble = 30.0
+var Effects = null
+var Explosion = load("res://Effects/explosion.tscn")
+
+func _physics_process(_delta):
+	position += direction
+	position.y = initial_position.y + sin(position.x/20) * wobble
+	if position.x > 1200: 
+		queue_free()
+
+
+func _ready():
+	initial_position.x = -100
+	initial_position.y = y_positions[randi() % y_positions.size()]
+	position = initial_position
 
 func _on_timer_timeout():
 	var Player = get_node_or_null("/root/Game/Player_Container/Player")
@@ -12,3 +31,21 @@ func _on_timer_timeout():
 		bullet.global_position = global_position + Vector2(0, -100).rotated(d)
 		Effects.add_child(bullet)
 		
+
+func damage(d):
+	health -= d
+	if health <= 0:
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instantiate()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+			hide()
+			await explosion.animation_finished
+		queue_free()
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		damage(100)
+		body.damage(100)
